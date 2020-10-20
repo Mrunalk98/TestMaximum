8a80440cdeccf8bb1ab2fac403108198d1fe34fb
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-20 21:18:50 +0530

    [mrunal] Add. Added a method to find maximum of 3 integers

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -7,6 +7,22 @@ namespace TestMaximum
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Test Maximum Program !");
+            int maxInteger = MaximumInteger(20, 10, 40);
+            Console.WriteLine("Maximum Integer Output: " + maxInteger);
+
         }
+
+        // UC 1
+        public static int MaximumInteger(int a, int b, int c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                return a;
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+                return b;
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
+                return c;
+            return 0;
+        }
+
     }
 }
