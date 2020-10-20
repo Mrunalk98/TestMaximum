9632b821fcfebae9d9bf90abf858c521473edfe2
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-20 23:14:40 +0530

    [mrunal] Refactor. Added all integer, float, string methods to one generic method to find maximum

---
+++ TestMaximum/GenericMaximum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum
+{
+    public class GenericMaximum <T> where T: IComparable
+    {
+        public T MaximumValue(T a, T b, T c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                return a;
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+                return b;
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
+                return c;
+            return default;
+        }
+    }
+}
--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -7,51 +7,17 @@ namespace TestMaximum
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Test Maximum Program !");
-            int maxInteger = MaximumInteger(20, 10, 40);
+
+            
+            int maxInteger = new GenericMaximum<int>().MaximumValue(20, 40, 10);
             Console.WriteLine("Maximum Integer Output: " + maxInteger);
 
-            double maxDouble = MaximumFloat(20.22, 3.058, 80.5);
+            double maxDouble = new GenericMaximum<double>().MaximumValue(20.22, 3.058, 80.5);
             Console.WriteLine("Maximum Double Output : " + maxDouble);
 
 
-            string maxString = MaximumString("Apple", "Peach", "Banana");
+            string maxString = new GenericMaximum<string>().MaximumValue("Apple", "Peach", "Banana");
             Console.WriteLine("Maximum String Output : " + maxString);
-        }
-
-        // UC 1
-        public static int MaximumInteger(int a, int b, int c)
-        {
-            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
-                return a;
-            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
-                return b;
-            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
-                return c;
-            return 0;
-        }
-
-        // UC 2
-        public static double MaximumFloat(double a, double b, double c)
-        {
-            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
-                return a;
-            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
-                return b;
-            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
-                return c;
-            return 0;
-        }
-
-        // UC 3
-        public static string MaximumString(string a, string b, string c)
-        {
-            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
-                return a;
-            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
-                return b;
-            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
-                return c;
-            return null;
         }
     }
 }
